86d51e5a1e6e67be0176084dbaf8548db898da3b
MZ <nikita.zholkovskyi@gmail.com>
2020-10-15 11:26:00 +0200

    made html and class post

---
+++ Post.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+class Post
+{
+    private $title;
+    private $date;
+    private $authorName;
+    private $content;
+
+
+
+    public function __construct(string $title, $date, string $authorName, string $content)
+    {
+
+        $this->title = $title;
+        $this->date = $date;
+        $this->authorName = $authorName;
+        $this->content = $content;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+
+        return $this->title;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+
+        return $this->content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorName(): string
+    {
+
+        return $this->authorName;
+    }
+}
--- index.php
+++ index.php
@@ -1,22 +1,72 @@
 <?php
+require "Post.php";
+require "PostLoader.php";
 //image this code could be a complex query
 $users = ['John Doe', 'Joe Doe', 'John Smith', 'An Onymous'];
 $articles = ['Terror over london', 'Football: a useless hobby?', 'Economic crisis ahead, says expert', 'Fortis is Fortwas'];
 //end controller
 //start view
+
+if (!isset($_POST['title'])) {
+$_POST['title'] = "";
+}
+
+if (!isset($_POST['date'])) {
+    $_POST['date'] = "";
+}
+if (!isset($_POST['name'])) {
+    $_POST['name'] = "";
+}
+if (!isset($_POST['text'])) {
+    $_POST['text'] = "";
+}
+$guestBook = new Post($_POST['title'], $_POST['date'], $_POST['name'], $_POST['text']);
+
+$title = $guestBook->getTitle();
+$date = date("Y/m/d h:i:sa");
+$name = $guestBook->getAuthorName();
+$message = $guestBook->getContent();
+$entryarray = ['message' => $message, 'name' => $name, 'date' => $date, 'title' => $title];
+var_dump($entryarray);
+
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My site</title>
+    <title>Guest Book</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+
+        body {
+            margin: auto;
+            width: 80%;
+    </style>
 </head>
 <body>
-    <h1>Welcome to my site!</h1>
-    <h2>Recent articles</h2>
-    <h2>Recent articles</h2>
+<form action="#" method="post">
+    <h1>Guest Book</h1>
+    <h3>Leave a message:</h3>
+    <section>
+        <label for="title">Title</label><br>
+        <input id="title" name="title" required>
+    </section>
+    <section>
+        <label for="name">Name</label><br>
+        <input id="name" name="name" required>
+    </section>
+    <section>
+        <label for="Text">Text</label><br>
+        <textarea id="text" name="text" rows="4" cols="50" required></textarea>
+    </section>
+    <input type="submit" name="submitButton">
+</form>
+
+
 </body>
 </html>
